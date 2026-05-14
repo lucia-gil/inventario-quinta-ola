3d85fb6c7887192f4ba4c07e2970dd1be4d1e419
Lucia tu pro <a20233424@pucp.edu.pe>
2026-05-14 02:02:58 -0500

    feat: CRUD items completo -GET, POST, PUT, disable funcionando con MySQL

--- backend/src/main/java/com/quintaola/util/DatabaseConnection.java
+++ backend/src/main/java/com/quintaola/util/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
 
     // CONECTAR: cambia estos datos si tu BD tiene usuario/contraseña diferente
-    private static final String URL      = "jdbc:mysql://localhost:3306/inventorydb?useSSL=false&serverTimezone=America/Lima";
+    private static final String URL = "jdbc:mysql://localhost:3306/inventorydb?useSSL=false&serverTimezone=America/Lima&allowPublicKeyRetrieval=true";
     private static final String USER     = "root";
     private static final String PASSWORD = "lucia1234"; // tu contraseña de MySQL
 
